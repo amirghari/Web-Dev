188fb6f6837a876502e9353f3b59ac7b1a0a8551
Amir Ghari <amir.ghari@metropolia.fi>
2023-08-29 10:02:29 +0300

    Expandable text

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
-import ListGroup from './Components/ListGroup'
-import Button from './Components/Button'
-import Alert from './Components/Alert'
-import { useState } from 'react'
+import ExText from './Components/ExText'
 
-function App() {
-  const items = ['Tehran', 'New York', 'Helsinki', 'Arak', 'Istanbul']
-  const [alertVisibility, setAlertVisibility] = useState(false)
-  const handleSelectItem = (items: string) => {
-    console.log(items)
-  }
+const App = () => {
   return (
-    <>
-      <div>
-        <ListGroup
-          items={items}
-          heading="Cities"
-          onSelectItem={handleSelectItem}
-        />
-      </div>
-      <Button
-        children="My Button"
-        color="primary"
-        onClick={() => setAlertVisibility(true)}
-      ></Button>
-      {alertVisibility && (
-        <Alert
-          children="My Alert"
-          onClick={() => setAlertVisibility(false)}
-        ></Alert>
-      )}
-    </>
+    <ExText
+      children="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi corrupti
+      architecto nam impedit corporis eligendi inventore pariatur, a
+      perspiciatis ea officia nobis totam perferendis eum mollitia eius
+      nostrum. Consectetur sequi inventore facilis libero, perferendis
+      voluptatum reiciendis doloremque laborum temporibus repellendus officia
+      quas. Officia fuga commodi omnis explicabo? Reprehenderit fuga culpa
+      accusantium nostrum! Saepe esse, a rerum obcaecati, molestias suscipit
+      ea aspernatur eveniet sed ipsum temporibus alias unde voluptas expedita
+      officia tenetur nemo quibusdam rem itaque eum praesentium minima?
+      Quibusdam eos voluptate quod alias? Recusandae in libero vel excepturi,
+      dolore optio facere earum, exercitationem ipsum praesentium harum quae
+      facilis consequuntur dolor."
+      charCount={500}
+    />
   )
 }
+
 export default App
