020e0e693722db2e63878cbe2034d35010501b4b
Amir Ghari <amir.ghari@metropolia.fi>
2023-08-31 09:02:06 +0300

    Getting form Values using hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,7 @@
-import ExText from './Components/ExText'
+import Form from './Components/Form'
 
 const App = () => {
-  return (
-    <ExText
-      children="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi corrupti
-      architecto nam impedit corporis eligendi inventore pariatur, a
-      perspiciatis ea officia nobis totam perferendis eum mollitia eius
-      nostrum. Consectetur sequi inventore facilis libero, perferendis
-      voluptatum reiciendis doloremque laborum temporibus repellendus officia
-      quas. Officia fuga commodi omnis explicabo? Reprehenderit fuga culpa
-      accusantium nostrum! Saepe esse, a rerum obcaecati, molestias suscipit
-      ea aspernatur eveniet sed ipsum temporibus alias unde voluptas expedita
-      officia tenetur nemo quibusdam rem itaque eum praesentium minima?
-      Quibusdam eos voluptate quod alias? Recusandae in libero vel excepturi,
-      dolore optio facere earum, exercitationem ipsum praesentium harum quae
-      facilis consequuntur dolor."
-      charCount={500}
-    />
-  )
+  return <Form></Form>
 }
 
 export default App
